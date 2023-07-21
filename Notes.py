e15560f77aa2bd586156269708d01ae0171a351f
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-07-21 20:16:45 +0800

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -8,4 +8,18 @@ async def download(url, filename):
     if response.status == 200:
         with open(filename, "wb") as f:
             f.write(await response.bytes())
-}
+#lib
+import piplite
+await piplite.install(['pandas'])
+await piplite.install(['matplotlib'])
+await piplite.install(['scipy'])
+await piplite.install(['scikit-learn'])
+await piplite.install(['skillsnetwork', 'seaborn'])
+
+#method
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import skillsnetwork
+import warnings
+warnings.filterwarnings('ignore')
