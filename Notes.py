50cc9f4af8ec8637197bbce51478f87641471b94
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-07-21 21:42:15 +0800

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -23,3 +23,56 @@ import matplotlib.pyplot as plt
 import skillsnetwork
 import warnings
 warnings.filterwarnings('ignore')
+from ipywidgets import interact, interactive, fixed, interact_manual
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+
+#function of plotting
+def DistributionPlot(RedFunction, BlueFunction, RedName, BlueName, Title):
+    width = 12
+    height = 10
+    plt.figure(figsize=(width, height))
+
+    ax1 = sns.displot(RedFunction, hist=False, color="r", label=RedName)
+    ax2 = sns.displot(BlueFunction, hist=False, color="b", label=BlueName, ax=ax1)
+
+    plt.title(Title)
+    plt.xlabel('Price (in dollars)')
+    plt.ylabel('Proportion of Cars')
+
+    plt.show()
+    plt.close()
+
+def PollyPlot(xtrain, xtest, y_train, y_test, lr,poly_transform):
+    width = 12
+    height = 10
+    plt.figure(figsize=(width, height))
+    
+    
+    #training data 
+    #testing data 
+    # lr:  linear regression object 
+    #poly_transform:  polynomial transformation object 
+ 
+    xmax=max([xtrain.values.max(), xtest.values.max()])
+
+    xmin=min([xtrain.values.min(), xtest.values.min()])
+
+    x=np.arange(xmin, xmax, 0.1)
+
+
+    plt.plot(xtrain, y_train, 'ro', label='Training Data')
+    plt.plot(xtest, y_test, 'go', label='Test Data')
+    plt.plot(x, lr.predict(poly_transform.fit_transform(x.reshape(-1, 1))), label='Predicted Function')
+    plt.ylim([-10000, 60000])
+    plt.ylabel('Price')
+    plt.legend()
+    
+#randomly split data into training and testing data using the function train_test_split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.10, random_state=1)
+print("number of test samples :", x_test.shape[0])
+print("number of training samples:",x_train.shape[0])
+
