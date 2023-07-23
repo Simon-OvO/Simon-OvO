719a59f131acc3f889fcfa1785f8502220009c5d
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-07-23 20:36:12 +0800

    Create Waffle.py

---
+++ Waffle.py
@@ -0,0 +1,19 @@
+!pip install pywaffle
+#import Waffle from pywaffle
+from pywaffle import Waffle
+
+#Set up the Waffle chart figure
+
+fig = plt.figure(FigureClass = Waffle,
+                 rows = 20, columns = 30, #pass the number of rows and columns for the waffle 
+                 values = df_dsn['Total'], #pass the data to be used for display
+                 cmap_name = 'tab20', #color scheme
+                 legend = {'labels': [f"{k} ({v})" for k, v in zip(df_dsn.index.values,df_dsn.Total)],
+                            'loc': 'lower left', 'bbox_to_anchor':(0,-0.1),'ncol': 3}
+                 #notice the use of list comprehension for creating labels 
+                 #from index and total of the dataset
+                )
+
+#Display the waffle chart
+plt.show()
+
