adc9cd5c34bf6bddd88f4aae02ae14424bc9b20d
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-07-28 12:27:07 +0800

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -35,7 +35,53 @@ data     'pandas'(openpyxl(read excel))
 analyse  'scikit-learn'
          'skillsnetwork'
          'seaborn'
-visualize'matplotlib'{Line：plt.plot(x, y, color='red', linewidth=2)
+visualize'matplotlib'x和y：表示标签或者位置，用来指定显示的索引，默认为None
+                     kind：表示绘图的类型，默认为line，折线图(line：折线图,bar/barh：柱状图（条形图），纵向/横向,pie：饼状图
+                                              hist：直方图（数值频率分布）,box：箱型图,kde：密度图，主要对柱状图添加Kernel 概率密度线
+                                              area：区域图（面积图）,scatter：散点图,hexbin：蜂巢图)
+                     ax：子图，可以理解成第二坐标轴，默认None
+                     subplots：是否对列分别作子图，默认False
+                     sharex：共享x轴刻度、标签。如果ax为None，则默认为True，如果传入ax，则默认为False
+                     sharey：共享y轴刻度、标签
+                     layout：子图的行列布局，(rows, columns)
+                     figsize：图形尺寸大小，(width, height)
+                     use_index：用索引做x轴，默认True
+                     title：图形的标题
+                     grid：图形是否有网格，默认None
+                     legend：子图的图例
+                     style：对每列折线图设置线的类型，list or dict
+                     logx：设置x轴刻度是否取对数，默认False
+                     logy
+                     loglog：同时设置x，y轴刻度是否取对数，默认False
+                     xticks：设置x轴刻度值，序列形式（比如列表）
+                     yticks
+                     xlim：设置坐标轴的范围。数值，列表或元组（区间范围）
+                     ylim
+                     rot：轴标签（轴刻度）的显示旋转度数，默认None
+                     fontsize : int, default None#设置轴刻度的字体大小
+                     colormap：设置图的区域颜色
+                     colorbar：柱子颜色
+                     position：柱形图的对齐方式，取值范围[0,1]，默认0.5（中间对齐）
+                     table：图下添加表，默认False。若为True，则使用DataFrame中的数据绘制表格
+                     yerr：误差线
+                     xerr
+                     stacked：是否堆积，在折线图和柱状图中默认为False，在区域图中默认为True
+                     sort_columns：对列名称进行排序，默认为False
+                     secondary_y：设置第二个y轴（右辅助y轴），默认为False
+                     mark_right : 当使用secondary_y轴时，在图例中自动用“(right)”标记列标签 ，默认True
+                     x_compat：适配x轴刻度显示，默认为False。设置True可优化时间刻度的显示
+                     1.2 其他常用说明
+                       color：颜色
+                       s：散点图大小，int类型
+                       设置x,y轴名称
+                      ax.set_ylabel(‘yyy’)
+                      ax.set_xlabel(‘xxx’)
+                     {df.plot(x=None, y=None, kind='line', ax=None, subplots=False, 
+                              sharex=None, sharey=False, layout=None, figsize=None, use_index=True, title=None, grid=None, legend=True, 
+                              style=None, logx=False, logy=False, loglog=False, xticks=None, yticks=None, xlim=None, ylim=None, rot=None, 
+                              fontsize=None, colormap=None, position=0.5, table=False, yerr=None, xerr=None, stacked=True/False, sort_columns=False, 
+                              secondary_y=False, mark_right=True, **kwds)
+                      Line：plt.plot(x, y, color='red', linewidth=2)
                       Area：plt.fill_between(x, y1, y2, color='blue', alpha=0.5)
                       Histogram：plt.hist(data, bins=10, color='orange', edgecolor='black')
                       Bar：plt.bar(x, height, color='green', width=0.5)
@@ -55,7 +101,27 @@ visualize'matplotlib'{Line：plt.plot(x, y, color='red', linewidth=2)
                   
 
          'seaborn'#countplot分类计数
-         'Foleyum'#交互
+         'Foleyum'#交互{# instantiate a feature group 
+                        aus_reg = folium.map.FeatureGroup()
+                        # Create a Folium map centered on Australia
+                        Aus_map = folium.Map(location=[-25, 135], zoom_start=4)
+                        # loop through the region and add to feature group
+                        for lat, lng, lab in zip(reg.Lat, reg.Lon, reg.region):
+                          aus_reg.add_child(
+                            folium.features.CircleMarker(
+                            [lat, lng],
+                            popup=lab,
+                            radius=5, # define how big you want the circle markers to be
+                            color='red',
+                            fill=True,
+                            fill_color='blue',
+                            fill_opacity=0.6
+                              )
+                            )
+                            }
+
+# add incidents to map
+Aus_map.add_child(aus_reg)}
          'Plotly'#交互{
                        
          }
@@ -164,3 +230,5 @@ Axes.set_xticks(ticks, labels=None, *, minor=False, **kwargs)#minor次要刻度
 Axes.grid(visible=None, which='major', axis='both', **kwargs)#网格线
 html.Div([html.Div()，html.Div()],style={"display":'flex'})#flex表示并列排列
 html.Br()#换行
+df['Month'] = pd.to_datetime(df['Date']).dt.month#提取月份
+df['Region'].unique()#唯一值，仅用于series
