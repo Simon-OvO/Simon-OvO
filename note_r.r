05ca104c735d1c143e906cbe78ce0b9a66522c42
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-08-02 22:58:19 +0800

    Create note_r.r

---
+++ note_r.r
@@ -0,0 +1,19 @@
+#快捷键 运行ctrl+shift+enter  运行单行ctrl+enter  打开新文件ctrl+shift+n
+ls()#show the names the objects saved in workspace
+library()#相当于import
+?log #help ducument
+args(log)#arguements of function
+class()#determine the type of object
+
+sqrt() log() exp()=e^x
+
+pi inf#无穷大
+
+library(dslabs)
+data('murders')#加载数据
+str()#显示结构describe()
+names()
+length()
+head()#first 6 rows
+murders$population#show the population size
+levels(factors)#分类
