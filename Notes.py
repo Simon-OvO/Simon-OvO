a814c0cbc6c09a59e687af325a4e03583052528d
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-08-04 01:48:55 +0800

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -114,6 +114,29 @@ visualize'matplotlib'x和y：表示标签或者位置，用来指定显示的索
                       Scatter：plt.scatter(x, y, color='purple', marker='o', s=50)
                       Subplotting：fig, axes = plt.subplots(nrows=2, ncols=2)
                       Customization: adding labels, title, legend, grid	Various customization	plt.title('Title')plt.xlabel('X Label')plt.ylabel('Y Label')plt.legend()plt.grid(True)
+                      pcolor:plt.pcolor(grouped_pivot, cmap='RdBu') #plt.colorbar()
+                             pcolormesh()
+#示例
+{fig, ax = plt.subplots()
+im = ax.pcolor(grouped_pivot, cmap='RdBu')
+
+#label names
+row_labels = grouped_pivot.columns.levels[1]
+col_labels = grouped_pivot.index
+
+#move ticks and labels to the center
+ax.set_xticks(np.arange(grouped_pivot.shape[1]) + 0.5, minor=False)
+ax.set_yticks(np.arange(grouped_pivot.shape[0]) + 0.5, minor=False)
+
+#insert labels
+ax.set_xticklabels(row_labels, minor=False)
+ax.set_yticklabels(col_labels, minor=False)
+
+#rotate label if too long
+plt.xticks(rotation=90)
+
+fig.colorbar(im)
+plt.show()}
                                                                                 
          'pandas'{Line：df.plot(x=’year’, y=’sales’, kind=’line’) 
                   Area：df.plot(kind='area')
