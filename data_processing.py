9dd5f5fef94443b437d44a460fea510bb4f5a75d
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-08-04 01:49:16 +0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -40,19 +40,27 @@ pandas.pivot_table()
 #空值(?/ /N/A)、重复值、异常值
 df.drop_duplicates(subset=None,keep='first',inplace=False)#去除重复值，subset列名
 df.info() #显示各列有多少非空值
-df.isnull()#显示哪些值为缺失值
+df.isnull()#布尔值表，值为True则为缺失值
 df.dropna()#去除空值
 df.fillna(0)#替换空值
 df.replace(missingvalue,newvalue)
-series.value_counts()#计算值频率
+series.value_counts()#计算值频率 返回唯一行数
+df[''].value_counts().idxmax()#计算最常见值
 (pandas.options.mode.use_inf_as_na = True#设置正负无穷为空值
 df.isin()#可用于自定义空值判断)
-df.astype(type) #当数据类型错误时转换数据类型,df.dtypes()查看数据类型
+df.astype(type) #当数据类型错误时转换数据类型,参数copy默认值False，df.dtypes()查看数据类型
+df.rename(columns={'':''}, inplace=True)#重命名列名
+
+#自定义排序
+df[].astype('category')
+cat_dtype = CategoricalDtype(categories=[2, 1], ordered=True)
+df[].astype(cat_dtype)
 
 #增加新列，调用pandas，numpy
 condlist=[dnp[:,0]=='Yearly',dnp[:,0]=='Monthly',dnp[:,0]=='Weekly']
 choicelist=[1,12,52]
 df['NormalizedAnnualCompensation']=pd.DataFrame(np.select(condlist,choicelist)*dnp[:,1])
+#选择多列df1=df[['','','']]
 
 #数据归一化
 1.min-max:0~1
@@ -64,6 +72,14 @@ group_names=[]
 df['binned']=pd.cut(df[],bins,labels=group_names,include_lowest=True)
 
 #分类变量转为定量变量
-pd.get_dummyies(df[])#生成数字类别，列名为类名，值为0，1
+d_pd.get_dummyies(df[])#生成类别df，列名为类名，值为0，1
+df = pd.concat([df, d_pd], axis=1)#连接表
+df.drop("", axis = 1, inplace=True)#删除列
 
+df.describe()#输出数据规格['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'],默认不包含object类型，可用参数include
+series.to_frame()
+df.groupby(['',''],as_index=False)
+df.get_group('')#获取分类数据
+df.pivot(index='',columns='')#透视图的columns为两列的数据
+MultiIndex.levels
 
