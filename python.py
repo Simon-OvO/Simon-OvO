ee17ac72ea6e986f1fd83eb41a2061902b3f877e
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-08-05 17:50:58 +0800

    Create python.py

---
+++ python.py
@@ -0,0 +1,37 @@
+#定义类Circle和
+import matplotlib.pyplot as plt
+%matplotlib inline  
+class Circle(object):
+    
+    # Constructor
+    def __init__(self, radius=3, color='blue'):
+        self.radius = radius
+        self.color = color 
+    
+    # Method
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        return(self.radius)
+    
+    # Method
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()  
+      
+circle1=Circle(10, 'red')
+dir(circle1)
+#错误处理
+a = 1
+try:b = int(input("Please enter a number to divide a"))
+    a = a/b
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+else:
+    print("success a=",a)
+finally:
+    print("Processing Complete")
