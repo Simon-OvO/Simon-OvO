5cac0669598014bc636588d77da369ccb9857095
Simon-OvO <139228201+Simon-OvO@users.noreply.github.com>
2023-08-07 16:36:15 +0800

    Update Mysql.sql

--- Mysql.sql
+++ Mysql.sql
@@ -4,7 +4,7 @@ select date_add(week(now()),interval n week)#日期加减
 select datediff(date1,date2)/timediff(time1,time2)#时间间隔
 select substr(salary,locate('-',salary)+1,length(salary)-locate('-',salary)-1)#数据清洗获取中间字段
 
-
+union 连接查询结果
 连接类型	说明
 INNER JOIN	（默认连接方式）只有当两个表都存在满足条件的记录时才会返回行。
 LEFT JOIN	返回左表中的所有行，即使右表中没有满足条件的行也是如此。
@@ -17,3 +17,7 @@ is null#空值判断不用=
 SUM(IF(rating < 3, 1, 0))
 sum(first=customer_pref_delivery_date)
 使用group by 后面几列都是以group by的列索引
+CASE
+    WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+    ELSE 'No'
+END AS 'triangle'
